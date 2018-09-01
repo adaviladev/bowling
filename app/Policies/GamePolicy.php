fb755af0389df814775b7918f7f1ea23b43bf8dc
adrian <adavila.dev@gmail.com>
2018-09-01 13:34:23 -0500

    added code coverage and commented out currently unnecessary lines

--- app/Policies/GamePolicy.php
+++ app/Policies/GamePolicy.php
@@ -17,10 +17,10 @@ class GamePolicy
      * @param  \App\Game  $game
      * @return mixed
      */
-    public function view(User $user, Game $game)
-    {
-        //
-    }
+    //public function view(User $user, Game $game)
+    //{
+    //    //
+    //}
 
     /**
      * Determine whether the user can create games.
@@ -28,10 +28,10 @@ class GamePolicy
      * @param  \App\User  $user
      * @return mixed
      */
-    public function create(User $user)
-    {
-        //
-    }
+    //public function create(User $user)
+    //{
+    //    //
+    //}
 
     /**
      * Determine whether the user can update the game.
@@ -40,10 +40,10 @@ class GamePolicy
      * @param  \App\Game  $game
      * @return mixed
      */
-    public function update(User $user, Game $game)
-    {
-        //
-    }
+    //public function update(User $user, Game $game)
+    //{
+    //    //
+    //}
 
     /**
      * Determine whether the user can delete the game.

175b60199ca600e617fd59fee95f5c89455c4c2c
Adrian Davila <adavila.dev@gmail.com>
2018-09-09 20:18:42 -0500

    resurrected old tests (#17)

--- app/Http/Controllers/RollsController.php
+++ app/Http/Controllers/RollsController.php
@@ -41,10 +41,10 @@ class RollsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+    //public function update(Request $request, $id)
+    //{
+    //    //
+    //}
 
     /**
      * Remove the specified resource from storage.
@@ -52,8 +52,8 @@ class RollsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
-        //
-    }
+    //public function destroy($id)
+    //{
+    //    //
+    //}
 }

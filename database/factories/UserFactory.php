ca785c218533f5ff47feb863fe33ec6a03605a8b
adrian <adavila.dev@gmail.com>
2019-02-26 21:35:00 -0600

    updated factory to pass

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,8 +15,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'first_name' => $faker->firstName,
-        'last_name' => $faker->lastName,
+        'first_name' => htmlspecialchars($faker->firstName),
+        'last_name' => htmlspecialchars($faker->lastName),
         'email' => $faker->unique()->safeEmail,
         'password' => Hash::make('secret'),
         'remember_token' => str_random(10),

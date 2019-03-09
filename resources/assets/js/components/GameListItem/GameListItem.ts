e8e93682d228d47ed464c969fb627344314b495b
adrian <adavila.dev@gmail.com>
2019-03-09 00:30:16 -0600

    created cards for list items

--- resources/assets/js/components/GameListItem/GameListItem.ts
+++ resources/assets/js/components/GameListItem/GameListItem.ts
@@ -1,14 +1,20 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
-import FramesTable from '../FramesTable/index.vue';
-
 @Component({
-  components: {
-    FramesTable,
-  },
   props: {
     game: Object,
   },
 })
-export default class GameListItem extends Vue {}
+export default class GameListItem extends Vue {
+  get performanceClass() {
+    if (this.$props.game.score >= 200) {
+      return 'bg-success';
+    }
+    if (this.$props.game.score >= 100) {
+      return 'bg-warning';
+    }
+
+    return 'bg-danger';
+  }
+}
--- resources/assets/js/components/GameListItem/index.vue
+++ resources/assets/js/components/GameListItem/index.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="flex-column">
-        <p>Game #{{ game.id }} - {{ game.score }} <small>created on {{ game.created_at }}</small></p>
-        <FramesTable
-            :frames="game.frames"
-        ></FramesTable>
-        <router-link
-            :to="{name: 'GameShow', params: {id: game.id}}"
+    <div class="card">
+        <div
+            :class="performanceClass"
+            class="card-header"
         >
-            View Game
-        </router-link>
+            <h5 class="text-center">{{ game.score }}</h5>
+        </div>
+        <div class="card-body clearfix">
+            <div class="card-text">
+                <p>Game #{{ game.id }} - <small>created on {{ game.created_at }}</small></p>
+            </div>
+            <router-link
+                class="btn btn-primary pull-right"
+                tag="button"
+                :to="{name: 'GameShow', params: {id: game.id}}"
+            >
+                View Game
+            </router-link>
+        </div>
     </div>
 </template>
 

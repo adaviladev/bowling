6d620b81caf167056a4563ac4b9b8770824620a8
Adrian Davila <adavila.dev@gmail.com>
2019-06-01 17:46:38 -0500

    updated build target based on environment and excluded js from PHP_CodeSniffer (#73)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 let path = require('path');
-let nodeExternals = require('webpack-node-externals');
+// let nodeExternals = require('webpack-node-externals');
 let isCoverage = process.env.NODE_ENV === 'coverage';
 
 /*
@@ -15,56 +15,56 @@ let isCoverage = process.env.NODE_ENV === 'coverage';
  */
 
 function resolve(dir) {
-  return path.join(__dirname, '/', dir);
+  return path.join(__dirname, '/', dir)
 }
 
 mix.ts('resources/assets/js/main.ts', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
+  .sass('resources/assets/sass/app.scss', 'public/css')
 
-   .webpackConfig({
-       output: {
-           // use absolute paths in sourcemaps (important for debugging via IDE)
-           devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-           devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
-       },
-     module: {
-       rules: [].concat(
-         {
-           test: /\.js$/,
-           loader: 'babel-loader',
-           exclude: /node_modules/,
-         },
-         // {
-         //   test: /\.vue$/,
-         //   loader: 'vue-loader',
-         // },
-           isCoverage ? {
-               test: /\.(vue|ts)/,
-               include: path.resolve('resources/assets/js'), // instrument only testing sources with Istanbul, after ts-loader runs
-               loader: 'istanbul-instrumenter-loader'
-           }: [],
-         // We're registering the TypeScript loader here. It should only
-         // apply when we're dealing with a `.ts` or `.tsx` file.
-         // {
-         //   test   : /\.tsx?$/,
-         //   loader : 'ts-loader',
-         //   exclude: /node_modules/,
-         //   options: {appendTsSuffixTo: [/\.vue$/]}
-         // }
-       )
-     },
+  .webpackConfig({
+    output: {
+      // use absolute paths in sourcemaps (important for debugging via IDE)
+      devtoolModuleFilenameTemplate        : '[absolute-resource-path]',
+      devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]',
+    },
+    module: {
+      rules: [].concat(
+        {
+          test   : /\.js$/,
+          loader : 'babel-loader',
+          exclude: /node_modules/,
+        },
+        // {
+        //   test: /\.vue$/,
+        //   loader: 'vue-loader',
+        // },
+        isCoverage ? {
+          test   : /\.(vue|ts)/,
+          include: path.resolve('resources/assets/js'), // instrument only testing sources with Istanbul, after ts-loader runs
+          loader : 'vue-loader',
+        } : [],
+        // We're registering the TypeScript loader here. It should only
+        // apply when we're dealing with a `.ts` or `.tsx` file.
+        {
+          test   : /\.tsx?$/,
+          loader : 'ts-loader',
+          exclude: /node_modules/,
+          options: {appendTsSuffixTo: [/\.vue$/]}
+        }
+      ),
+    },
 
-     resolve: {
-       // We need to register the `.ts` extension so Webpack can resolve
-       // TypeScript modules without explicitly providing an extension.
-       // The other extensions in this list are identical to the Mix
-       // defaults.
-       extensions: ['*', '.js', '.jsx', '.vue', '.ts', '.tsx'],
-       alias: {
-         '@': resolve('resources/assets/js')
-       }
-     },
-       target: 'node',  // webpack should compile node compatible code
-       externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-       devtool: "inline-cheap-module-source-map"
-   })
+    resolve  : {
+      // We need to register the `.ts` extension so Webpack can resolve
+      // TypeScript modules without explicitly providing an extension.
+      // The other extensions in this list are identical to the Mix
+      // defaults.
+      extensions: ['*', '.js', '.jsx', '.vue', '.ts', '.tsx'],
+      alias     : {
+        '@': resolve('resources/assets/js'),
+      },
+    },
+    target   : isCoverage ? 'node' : 'web',  // webpack should compile node compatible code
+    // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    devtool  : 'inline-cheap-module-source-map',
+  })

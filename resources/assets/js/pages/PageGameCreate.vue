c52922348a2b33fb217aa723587b2ae69e82895c
adrian <adavila.dev@gmail.com>
2019-08-13 18:46:39 -0500

    updated factories; simplified GamesController@store method

--- resources/assets/js/pages/PageGameCreate.vue
+++ resources/assets/js/pages/PageGameCreate.vue
@@ -1,9 +1,23 @@
 <template>
-
+    <div>
+        <h1>Create a Game</h1>
+        <GameEditor/>
+    </div>
 </template>
 
-<script>
-  export default {}
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import GameEditor from '@/components/GameEditor.vue';
+
+@Component({
+  components: {
+    GameEditor,
+  }
+})
+export default class PageGameCreate extends Vue
+{
+
+}
 </script>
 
 <style scoped>

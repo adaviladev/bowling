fbdd3ac905ceb101ffe69211eeb23dc7194d4f4c
adrian <adavila.dev@gmail.com>
2019-12-27 13:31:47 -0600

    moved api routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
+Auth::routes();
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'users' => 'UsersController',
+    'games' => 'GamesController',
+    'games/{game}/rolls' => 'RollsController'
+]);

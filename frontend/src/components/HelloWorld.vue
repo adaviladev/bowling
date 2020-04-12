8a8b0de7773e83cfa16f3c021434e3d110af554c
adrian <adavila.dev@gmail.com>
2020-04-12 16:38:44 -0500

    removed ts from HelloWorld component tree

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -98,16 +98,14 @@
     </div>
 </template>
 
-<script lang="ts">
-import {
-  Component,
-  Prop,
-  Vue
-} from "vue-property-decorator";
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    }
+  }
 }
 </script>
 
